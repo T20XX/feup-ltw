4ad456dc0081bc5303b53af50e1ce90462fc0068
Cat <catramos96@gmail.com>
2016-11-24 10:53:11 +0000

    Base de Dados

--- ltw.sql
+++ ltw.sql
@@ -1,15 +1,15 @@
 PRAGMA foreign_keys = ON;
 
 
-drop table if exists account;
-drop table if exists restaurant;
-drop table if exists functionTime;
-drop table if exists review;
-drop table if exists reply;
-drop table if exists photo;
+drop table if exists Account;
+drop table if exists Restaurant;
+drop table if exists FunctionTime;
+drop table if exists Review;
+drop table if exists Reply;
+drop table if exists Photo;
 
 
-CREATE TABLE account (
+CREATE TABLE Account (
 	id_account VARCHAR PRIMARY KEY,					
 	name VARCHAR NOT NULL,
 	pass VARCHAR NOT NULL,
@@ -17,17 +17,17 @@ CREATE TABLE account (
 );
 
 
-CREATE TABLE restaurant (
+CREATE TABLE Restaurant (
 	id_restaurant INTEGER PRIMARY KEY AUTOINCREMENT,
-	id_owner INTEGER REFERENCES account(id_account),
-	id_functionTime INTEGER REFERENCES functionTime(id_functionTime),
+	id_owner INTEGER REFERENCES Account(id_account),
+	id_functionTime INTEGER REFERENCES FunctionTime(id_functionTime),
 	name VARCHAR NOT NULL,
 	address VARCHAR NOT NULL,
 	description VARCHAR,
 	stars REAL 
 );
 
-CREATE TABLE functionTime (
+CREATE TABLE FunctionTime (
 	id_functionTime INTEGER PRIMARY KEY AUTOINCREMENT,
 	open_time VARCHAR NOT NULL,
 	close_time VARCHAR NOT NULL,
@@ -41,23 +41,47 @@ CREATE TABLE functionTime (
 );
 
 
-CREATE TABLE review (
+CREATE TABLE Review (
 	id_review INTEGER PRIMARY KEY AUTOINCREMENT,
-	id_restaurant INTEGER REFERENCES restaurant(id_restaurant),
-	id_reviewer INTEGER REFERENCES account(id_account),
+	id_restaurant INTEGER REFERENCES Restaurant(id_restaurant),
+	id_reviewer INTEGER REFERENCES Account(id_account),
 	comment VARCHAR,
 	score INTEGER NOT NULL
 );
 
 
-CREATE TABLE reply (
+CREATE TABLE Reply (
 	id_reply INTEGER PRIMARY KEY AUTOINCREMENT,
-	id_review INTEGER REFERENCES review(id_review),
+	id_review INTEGER REFERENCES Review(id_review),
 	comment VARCHAR NOT NULL	
 );
 	
-CREATE TABLE photo (
+CREATE TABLE Photo (
 	id_photo INTEGER PRIMARY KEY AUTOINCREMENT,
-	id_restaurant INTEGER REFERENCES restaurant(id_restaurant),
+	id_restaurant INTEGER REFERENCES Restaurant(id_restaurant),
 	path VARCHAR NOT NULL
 );
+
+CREATE TABLE Type {
+id_restaurant INTEGER PRIMARY KEY Restaurant(id_restaurant),
+id_category VARCHAR PRIMARY KEY Category(id_category)
+}
+
+CREATE TABLE Category {
+id_category VARCHAR PRIMARY KEY
+}
+
+
+INSERT INTO Category VALUES ('Chinese');
+INSERT INTO Category VALUES ('Sushi');
+INSERT INTO Category VALUES ('Italian');
+INSERT INTO Category VALUES ('French');
+INSERT INTO Category VALUES ('Mexican');
+INSERT INTO Category VALUES ('Indian');
+INSERT INTO Category VALUES ('Fast Food');
+INSERT INTO Category VALUES ('Snack Bar');
+INSERT INTO Category VALUES ('Brazilian');
+INSERT INTO Category VALUES ('Coffee Shop');
+INSERT INTO Category VALUES ('Pastry Shop');
+INSERT INTO Category VALUES ('Sea Food');
+
--- signin.php
+++ signin.php
@@ -1,6 +1,6 @@
 <?php
 	$db = new PDO('sqlite:ltw.db');
-	$stmt = $db->prepare('SELECT id_account FROM account WHERE id_account = ? AND pass = ?');
+	$stmt = $db->prepare('SELECT id_account FROM Account WHERE id_account = ? AND pass = ?');
 	$stmt->execute(array($_POST['username'],sha1($_POST['password'])));
 	$result = $stmt->fetchAll();
 	if ($result != null){
