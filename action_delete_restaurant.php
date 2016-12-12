e0cbe8f722efc0eb13b34bd6dad0f5fb14d5f90f
Cat <catramos96@gmail.com>
2016-12-12 21:46:46 +0000

    JQuery para replies e comments

--- action_delete_restaurant.php
+++ action_delete_restaurant.php
@@ -1,3 +1,11 @@
 <?php
+	include_once('database/connection.php');
+	include_once('database/account.php');
+	include_once('database/restaurant.php');
+	session_start();
+	deleteRestaurants($db,$_SESSION['id_account']);
+	deleteAccount($db,$_SESSION['id_account']);
+	session_destroy();
 
+	header('Location: my_restaurants.php');
 ?>
