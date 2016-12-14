a4a9f02cc6734ab14f26436062b9b5036ec04719
Mario Fernandes <mario.gustavo@hotmail.com>
2016-12-14 23:03:12 +0000

    lockout

---
+++ lockout.php
@@ -0,0 +1,6 @@
+<?php
+include_once('templates/session_start.php');
+include ('templates/header.php');
+include('templates/lockout.php');
+include ('templates/footer.php');
+?>
--- templates/feed_content.php
+++ templates/feed_content.php
@@ -3,6 +3,7 @@
 	<h1 class="title"> Top 10 Restaurants </h1>
 	<div class="display_big_center">
 		<?php
+		
 			$result = getTop10Restaurant($db);
 			foreach( $result as $row) {
 				echo '<a class="item" href="restaurant_item.php?id=' . $row['id_restaurant'] . '">' . $row['name']  .  '</a>';
